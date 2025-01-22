0b32fb7c8697c031096c3c377aebf7936aa81201
henry <daluegonwanne@gmail.com>
2025-01-21 21:10:25 -0500

    added gitignore file and updated main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "ansys-terraform-state-bucket"
     key            = "ansys-vpc/terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-east-1"
     dynamodb_table = "ansys-lock-table"
     encrypt        = true
   }
